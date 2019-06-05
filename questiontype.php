3055e8c4f4d2c5adbd8a63efbfad4747f3762698
André Camacho <acamacho@Andres-MacBook-Pro.local>
2019-06-05 14:37:31 +0200

    extend question_type directly instead of qtype_rgessay

--- questiontype.php
+++ questiontype.php
@@ -25,12 +25,20 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-require_once($CFG->dirroot . '/question/type/essay/questiontype.php');
+require_once($CFG->libdir . '/questionlib.php');
 
 /**
  * The essay question type.
  */
-class qtype_rgessay extends qtype_essay {
+class qtype_rgessay extends question_type {
+
+    public function is_manual_graded() {
+        return true;
+    }
+
+    public function response_file_areas() {
+        return array('attachments', 'answer');
+    }
 
     public function get_question_options($question) {
         global $DB;
@@ -73,5 +81,112 @@ class qtype_rgessay extends qtype_essay {
         $options->responsetemplateformat = $formdata->responsetemplate['format'];
         $DB->update_record('qtype_essay_options', $options);
     }
+
+    protected function initialise_question_instance(question_definition $question, $questiondata) {
+        parent::initialise_question_instance($question, $questiondata);
+        $question->responseformat = $questiondata->options->responseformat;
+        $question->responserequired = $questiondata->options->responserequired;
+        $question->responsefieldlines = $questiondata->options->responsefieldlines;
+        $question->attachments = $questiondata->options->attachments;
+        $question->attachmentsrequired = $questiondata->options->attachmentsrequired;
+        /*
+        $question->graderinfo = $questiondata->options->graderinfo;
+        $question->graderinfoformat = $questiondata->options->graderinfoformat;
+        */
+        $question->responsetemplate = $questiondata->options->responsetemplate;
+        $question->responsetemplateformat = $questiondata->options->responsetemplateformat;
+        $filetypesutil = new \core_form\filetypes_util();
+        $question->filetypeslist = $filetypesutil->normalize_file_types($questiondata->options->filetypeslist);
+    }
+
+    public function delete_question($questionid, $contextid) {
+        global $DB;
+
+        $DB->delete_records('qtype_rgessay_options', array('questionid' => $questionid));
+        parent::delete_question($questionid, $contextid);
+    }
+
+    /**
+     * @return array the different response formats that the question type supports.
+     * internal name => human-readable name.
+     *
+     * @throws coding_exception
+     */
+    public function response_formats() {
+        return array(
+            'editor' => get_string('formateditor', 'qtype_essay'),
+            'editorfilepicker' => get_string('formateditorfilepicker', 'qtype_essay'),
+            'plain' => get_string('formatplain', 'qtype_essay'),
+            'monospaced' => get_string('formatmonospaced', 'qtype_essay'),
+            'noinline' => get_string('formatnoinline', 'qtype_essay'),
+        );
+    }
+
+    /**
+     * @return array the choices that should be offered when asking if a response is required
+     * @throws coding_exception
+     */
+    public function response_required_options() {
+        return array(
+            1 => get_string('responseisrequired', 'qtype_essay'),
+            0 => get_string('responsenotrequired', 'qtype_essay'),
+        );
+    }
+
+    /**
+     * @return array the choices that should be offered for the input box size.
+     * @throws coding_exception
+     */
+    public function response_sizes() {
+        $choices = array();
+        for ($lines = 5; $lines <= 40; $lines += 5) {
+            $choices[$lines] = get_string('nlines', 'qtype_essay', $lines);
+        }
+        return $choices;
+    }
+
+    /**
+     * @return array the choices that should be offered for the number of attachments.
+     * @throws coding_exception
+     */
+    public function attachment_options() {
+        return array(
+            0 => get_string('no'),
+            1 => '1',
+            2 => '2',
+            3 => '3',
+            -1 => get_string('unlimited'),
+        );
+    }
+
+    /**
+     * @return array the choices that should be offered for the number of required attachments.
+     * @throws coding_exception
+     */
+    public function attachments_required_options() {
+        return array(
+            0 => get_string('attachmentsoptional', 'qtype_essay'),
+            1 => '1',
+            2 => '2',
+            3 => '3'
+        );
+    }
+
+    /*
+
+    public function move_files($questionid, $oldcontextid, $newcontextid) {
+        parent::move_files($questionid, $oldcontextid, $newcontextid);
+        $fs = get_file_storage();
+        $fs->move_area_files_to_new_context($oldcontextid,
+            $newcontextid, 'qtype_rgessay', 'graderinfo', $questionid);
+    }
+
+    protected function delete_files($questionid, $contextid) {
+        parent::delete_files($questionid, $contextid);
+        $fs = get_file_storage();
+        $fs->delete_area_files($contextid, 'qtype_essay', 'graderinfo', $questionid);
+    }
+
+    */
 
 }
